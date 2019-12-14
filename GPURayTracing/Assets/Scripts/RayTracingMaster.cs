4807bb4e528031a37fc5039565ea486d477a194a
ThatFinnishGuy <ilari.saarenpaa@gmail.com>
2019-12-14 14:36:44 +0200

    Changed Build Settings

--- GPURayTracing/Assets/Scripts/RayTracingMaster.cs
+++ GPURayTracing/Assets/Scripts/RayTracingMaster.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 /* GPU Ray Tracing implementation as seen in http://blog.three-eyed-games.com/2018/05/03/gpu-ray-tracing-in-unity-part-1/ */
 
